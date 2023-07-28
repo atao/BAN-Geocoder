45a4132aa000421e4f4977bfc20adbdeb16233ba
atao <atao@users.noreply.github.com>
2023-07-28 15:17:24 +0200

    Update ban_geocoder.py

--- ban_geocoder.py
+++ ban_geocoder.py
@@ -1,133 +1,70 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
 
-# Geocoding addresses using the national address database API - https://adresse.data.gouv.fr/ - BAN 
-
-import argparse
-import sys
-import requests
 import json
+import sys
+import urllib.parse
 
-def geocodage(address):
+import click
+import requests
+
+
+def geocodage(verbose, address):
     try:
-        r = requests.get('https://api-adresse.data.gouv.fr/search/?q='+address)
-        global json_data
+        if verbose:
+            print("[+] Geocoding address...")
+        address = urllib.parse.quote(address)
+        r = requests.get('https://api-adresse.data.gouv.fr/search/?q=' + address)
         json_data = json.loads(r.text)
-        return json_data
+        results = []
+        if json_data is not None:
+            i = 0
+            for _ in json_data["features"]:
+                tmp = {"address": json_data["features"][i]["properties"]["label"],
+                       "score": json_data["features"][i]["properties"]["score"],
+                       "latitude": json_data["features"][i]["geometry"]["coordinates"][0],
+                       "longitude": json_data["features"][i]["geometry"]["coordinates"][1]}
+                # results.append(json_data["features"][i]["properties"]["label"])
+                data = str(tmp["address"]) + ";" + str(tmp["score"]) + ";" + str(tmp["latitude"]) + ";" + str(
+                    tmp["longitude"])
+                results.append(data)
+                i = i + 1
+        return results
     except:
         print("Error")
 
-def reverse(lat, lon):
-    try:
-        r = requests.get('https://api-adresse.data.gouv.fr/reverse/?lon='+lon+'&lat='+lat)
-        global json_data
-        json_data = json.loads(r.text)
-        return json_data
-    except:
-        print("Error")
 
-def parse_geocodage(address):
-    try:
-        json_data = geocodage(address)
-    except:
-        print("Enable to geocode data...")
-        sys.exit()
-    try:
-        adr = json_data["features"][0]["properties"]["label"]
-        return(adr)
-    except:
-        print("error parsing data")
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.option('--verbose', '-v', is_flag=True, help="Verbose mode")
+@click.option('--address', '-a', help='Address to be geocoded', required=True, type=str)
+@click.option('--nb', '-n', help='Number of results to return  [default=1]', default=0, show_default=False, type=int)
+def cli(verbose, address, nb):
+    if verbose:
+        print("Geocoding addresses using the national address database API - https://adresse.data.gouv.fr/ - BAN")
+    address = geocodage(verbose, address)
+    if address is not None:
+        nbresults = len(address)
+        i = 0
+        if nb!=0:
+            nb = nb - 1
+        if verbose:
+            print("{:10} {:10} {:18} {:10}".format("Longitude", "Latitude", "Score", "Address"))
+        for _ in address:
+            print("{:10} {:10} {:10} {:10}".format(address[i].split(";")[3], address[i].split(";")[2],
+                                                   address[i].split(";")[1], address[i].split(";")[0]))
+            if nb == nbresults:
+                pass
+            elif nb > nbresults:
+                nb = nbresults
+            elif nb == i:
+                break
+            else:
+                pass
+            i = i + 1
 
-def parse_reverse(lat, lon):
-    try:
-        json_data = reverse(lat, lon)
-    except:
-        print("Enable to reverse data...")
-        sys.exit()
-    try:
-        label = json_data["features"][0]["properties"]["label"]
-        return label
-    except:
-        print("Parsing reverse error")
 
-def csv(json_data):
-    try:
-        adr = json_data["features"][0]["properties"]["housenumber"]
-        adr += ";"
-    except:
-        print("No housenumber!")
-        adr = ""
-    try:
-        adr += json_data["features"][0]["properties"]["street"]
-        adr += ";"
-    except:
-        print("No Street!")
-        adr = ""
-    try:
-        adr += json_data["features"][0]["properties"]["postcode"]
-        adr += ";"
-        adr += json_data["features"][0]["properties"]["city"]
-        adr += ";"
-        # adr += str(json_data["features"][0]["properties"]["citycode"])
-        # adr += ";"
-        adr += gps(json_data)
-        return(adr)
-    except:
-        print("error parsing data")
-
-def gps(json_data):
-    try:
-        c_gps = str(json_data["features"][0]["geometry"]["coordinates"])
-        c_gps = c_gps.replace("[","").replace("]","")
-        return(c_gps)
-    except:
-        print("error parsing coordinates")
-
-def score(json_data):
-    try:
-        score = json_data["features"][0]["properties"]["score"]
-        return (score)
-    except:
-        print("error parsing coordinates")
-
-def version():
-    print ("hello, i'm in version one")
-
-parser = argparse.ArgumentParser(description="Geocoding addresses using the national address database API - https://adresse.data.gouv.fr/ - BAN \nThe data come from IGN, La Poste, DGFiP, Etalab and OpenStreetMap France.")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-parser.add_argument("-a", "--address", type=str, help="enter the address to geocoder")
-parser2 = parser.add_argument_group()
-parser2.add_argument("--lat", "--latitude", type=str, help="enter Latitude")
-parser2.add_argument("--lon", "--longitude", type=str, help="enter Longitude")
-parser.add_argument("-s", "--score", action="store_true", default=False, help="show score")
-parser.add_argument("-g", "--gps", action='store_true', default=False, help="show coordinates")
-parser.add_argument("--csv", action='store_true', default=False, help="show all in csv format")
-parser.add_argument("--version", action="store_true", default=False, help="show version")
-
-args = parser.parse_args()
-
-# print(args) # affichage du dictionnaire pour mise au point
-
-if len(sys.argv) == 1:
-    print("Specify arguments. Pass --help or -h to display help")
-    sys.exit()
-if args.version:
-    version()
-    sys.exit()
-if args.address:
-    print(parse_geocodage(args.address))
-if (args.lat and not args.lon) or (args.lon and not args.lat):
-    print("Please indicate latitude and longitude, not just one of them.")
-    sys.exit()
-if args.lat and args.lon:
-    print(parse_reverse(args.lat, args.lon))
-if json_data["features"] == []:
-    print("Exiting...")
-    sys.exit()
-if args.gps:
-    print("Coordinates :",gps(json_data))
-if args.score:
-    print("Score :", score(json_data))
-if args.csv:
-    print(csv(json_data))
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        cli.main(['--help'])
+    else:
+        cli()
