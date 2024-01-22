4f1f0819b91ac0f702fdf97d7d473bc29c61ef52
ATAO <atao@users.noreply.github.com>
2024-01-22 17:17:41 +0100

    Add local geocoding

--- geocoder/geocoder.py
+++ geocoder/geocoder.py
@@ -4,6 +4,8 @@ import urllib.parse
 import pandas as pd
 import requests
 
+from .addresses_matcher import AddressesMatcher
+
 
 def perform_geocoding(address, limit, verbose):
     """
@@ -60,3 +62,51 @@ def perform_geocoding(address, limit, verbose):
         return geocoded
     else:
         return None
+
+
+def local_geocoding(input_file, database, processes, verbose):
+    """
+    Perform local geocoding on a set of addresses using a local Base Adresse Nationale (BAN) database.
+
+    This function reads a file containing addresses, geocodes each address using the local BAN database,
+    and returns a DataFrame with the original addresses and their corresponding latitude and longitude.
+
+    Parameters:
+    - input_file (str): The file path of the text file containing addresses to geocode. Each address
+                        should be on a separate line.
+    - database (str): The file path of the SQLite database containing the BAN data.
+    - processes (int): The number of worker processes to use for multiprocessing.
+    - verbose (bool): If set to True, additional information will be printed to the console during execution.
+
+    Returns:
+    - geocoded (DataFrame): A pandas DataFrame with columns 'Address', 'Latitude', and 'Longitude'.
+                            Each row contains the original address and its geocoded coordinates.
+    """
+    # Initialize the AddressesMatcher class with the provided database, number of processes, and verbosity
+    matcher = AddressesMatcher(database=database, num_processes=processes, verbose=verbose)
+
+    # Use the geocode_addresses method to geocode the addresses from the input file
+    addresses_geocoded = matcher.geocode_addresses(input_file=input_file)
+
+    # Prepare lists to store individual latitude and longitude values
+    latitudes = []
+    longitudes = []
+
+    # Obtain the list of addresses from the geocoded results
+    addresses = list(addresses_geocoded.keys())
+
+    # Iterate through the geocoded coordinates and split them into latitude and longitude
+    for coords in addresses_geocoded.values():
+        lat, lon = coords.split(', ')
+        latitudes.append(lat)
+        longitudes.append(lon)
+
+    # Create a DataFrame with the addresses and their corresponding latitude and longitude
+    geocoded = pd.DataFrame({
+        'Address': addresses,
+        'Latitude': latitudes,
+        'Longitude': longitudes
+    })
+
+    # Return the DataFrame containing the geocoded data
+    return geocoded
